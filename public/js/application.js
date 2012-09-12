b6f5a660b55d2f62f0e990ff7514e650c1e9832a
sixofhearts <paul@sixofhearts.us>
2012-09-12 01:27:44 -0700

    Added dropdowns.

---
+++ public/js/application.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#about-link").parent().hover(function() {
+	$("#about-link").addClass("selected");
+	$("#about-dropdown").show();
+    }, function() {
+	$("#about-link").removeClass("selected");
+	$("#about-dropdown").hide();
+    });
+    $("#programs-link").parent().hover(function() {
+	$("#programs-link").addClass("selected");
+	$("#programs-dropdown").show();
+    }, function() {
+	$("#programs-link").removeClass("selected");
+	$("#programs-dropdown").hide();
+    });
+});
