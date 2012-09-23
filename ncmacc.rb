a42c4036014bbcf962a7e2b961717c75408e5f59
Paul S. Chun <paul@sixofhearts.us>
2012-09-23 13:42:32 -0700

    Switched program column order.

--- ncmacc.rb
+++ ncmacc.rb
@@ -47,13 +47,13 @@ MUSIC_PROGRAMS = [["Studio Program", "studio_program"],
 PERFORMING_ARTS_PROGRAMS = [["Korean-American Youth Musical Group", "korean_american_youth_musical_group"]]
 #                            ["Fillmore Youth, Drum and Dance", "fillmore_youth_drum_and_dance"]]     
 
-WREACH_OUT_PROGRAMS = [["Children's Chorus", "childrens_chorus"],
+COMMUNITY_PROGRAMS = [["Children's Chorus", "childrens_chorus"],
                        ["Group Art Lessons", "group_art_lessons"],
                        ["Group Piano Lessons", "group_piano_lessons"],
                        ["Taekwondo", "taekwondo"],
                        ["Youth Development", "youth_development"]]
 
 
-PROGRAMS = [["Music", MUSIC_PROGRAMS, "For musicians of all ages."],
-            ["Performing Arts", PERFORMING_ARTS_PROGRAMS, "For young performers."],
-            ["WREACH OUT", WREACH_OUT_PROGRAMS, "Free programs for community youths."]]
+PROGRAMS = [["Community", COMMUNITY_PROGRAMS, "Free programs for community youths."],
+            ["Music", MUSIC_PROGRAMS, "For musicians of all ages."],
+            ["Performing Arts", PERFORMING_ARTS_PROGRAMS, "For young performers."]]
